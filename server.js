17819f31468f39f031d75b494e978878bac7d853
artbohr <bohrcs@gmail.com>
2018-03-13 01:53:23 +0100

    Add routes and scheme

--- server.js
+++ server.js
@@ -18,11 +18,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  exercises: [{
+    description: String,
+    duration: Number
+  }]
+});
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+const User = mongoose.model("User", userSchema);
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
@@ -42,6 +49,32 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+// create new user
+app.post('/api/exercise/new-user', (req, res) =>{
+
+  const userForm = new User({username: req.body.username});
+  userForm.save();
+
+  res.send({"username": userForm.username, "_id" : userForm._id})
+});
+
+// route for appending exercises
+
+// route for finding a user by id
+
+// get all users
+app.get('/api/exercise/users', (req, res) => {
+  User.find({})
+    .then(doc=>res.json(doc)
+    ).catch(err=> res.send({message: err}))
+});
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
